134f4f7c89c891180640cea3946eb860c683db47
siba-dev <commits@siba.dev>
2025-12-29 23:58:14 +0100

    Improved help message

--- src/main/kotlin/de/siba/reportbookgen/ReportBookGenerator.kt
+++ src/main/kotlin/de/siba/reportbookgen/ReportBookGenerator.kt
@@ -22,21 +22,28 @@ fun main(args: Array<String>) {
 class ReportBookGenerator : CliktCommand() {
     private val logger: Logger = LoggerFactory.getLogger(this.javaClass)!!
 
-    val inputDir by option(names = arrayOf("-i", "--inDirectory")).path(
+    val inputDir by option(names = arrayOf("-i", "--inDirectory"), help = "Directory to get JSON files from.").path(
         mustExist = true, canBeFile = false, mustBeReadable = true
     )
         .required()
-    val outputDir by option(names = arrayOf("-o", "--outDirectory")).path( //
+    val outputDir by option(
+        names = arrayOf("-o", "--outDirectory"), help = "Directory to write generated word files to."
+    ).path( //
         canBeFile = false, mustBeWritable = true
     )
         .required()
 
-    val yearMap by option(names = arrayOf("-y", "--year")).associate(transform = {
+    val yearMap by option(
+        names = arrayOf("-y", "--year"), help = "Apprenticeship year starts. Example: -y 1=2023-08-29"
+    ).associate(transform = {
         Pair(
             it.first.toInt(), LocalDate.parse(it.second)
         )
     })
-    val templateMap by option(names = arrayOf("-t", "--template")).associate(transform = {
+    val templateMap by option(
+        names = arrayOf("-t", "--template"),
+        help = "Defines which template to use for each year. This feature is available since some templates have a fixed year choice that can not be templated. Example: -t \"1= ~/template1.docx\""
+    ).associate(transform = {
         Pair(
             it.first.toInt(), Path.of(it.second)
         )
